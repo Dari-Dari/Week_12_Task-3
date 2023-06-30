c400bfe6b5393a78310fd998539760a3dbfc777c
Daria Fadeeva <d9119046505@gmail.com>
2023-07-01 00:50:42 +0300

    fix if

--- imt.js
+++ imt.js
@@ -7,11 +7,11 @@ function calculateBmi() {
     const weight = parseFloat(weightInput.value);
     const height = parseFloat(heightInput.value);
 
-    if (isNaN(weight) || isNaN(height) || weight < 20 || weight > 500 || height < 100 || height > 250) {
+    if (weight < 20 || weight > 500 || height < 100 || height > 250) {
         result.textContent = 'Введите корректные значения для веса и роста!';
         return;
     }
-    
+
     const calcBmi = weight / Math.pow(height / 100, 2);
     result.textContent = `Ваш ИМТ: ${calcBmi.toFixed(2)}`;
 }
